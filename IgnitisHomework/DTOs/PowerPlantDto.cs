79389f174fb17dd06b40d9db5f0047b997ca9a31
Tadas Maksimavičius <tadasmak@gmail.com>
2025-11-11 00:24:29 +0200

    Add decimal points to Power ranges for correct decimal point validation in PowerPlant model and PowerPlantDto

--- IgnitisHomework/DTOs/PowerPlantDto.cs
+++ IgnitisHomework/DTOs/PowerPlantDto.cs
@@ -12,10 +12,10 @@ namespace IgnitisHomework.DTOs
         public string Owner { get; set; } = string.Empty;
 
         [Required]
-        [Range(0, 200, ErrorMessage = "Power must be between 0 and 200.")]
+        [Range(0.0, 200.0, ErrorMessage = "Power must be between 0 and 200.")]
         public double? Power { get; set; }
 
-        [Required] public DateTime ValidFrom { get; set; }
+        [Required] public DateTime? ValidFrom { get; set; }
         public DateTime? ValidTo { get; set; }
 
         public static PowerPlantDto FromEntity(PowerPlant entity)
--- IgnitisHomework/Models/PowerPlant.cs
+++ IgnitisHomework/Models/PowerPlant.cs
@@ -11,10 +11,10 @@ namespace IgnitisHomework.Models
         public string Owner { get; set; } = string.Empty;
 
         [Required]
-        [Range(0, 200, ErrorMessage = "Power must be between 0 and 200.")]
+        [Range(0.0, 200.0, ErrorMessage = "Power must be between 0 and 200.")]
         public double Power { get; set; }
 
-        [Required] public DateTime ValidFrom { get; set; }
+        [Required] public DateTime? ValidFrom { get; set; }
         public DateTime? ValidTo { get; set; }
     }
 }
